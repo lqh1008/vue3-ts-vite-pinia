d4e099c5be66e906d1e5559efee428ab5a609544
lvqihang <lvqh@asiainfo.com>
2022-03-17 19:36:55 +0800

    fix: eslint禁止非空断言处理

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,6 +31,7 @@ module.exports = {
     }
   ],
   rules: {
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off'
   }
 }
